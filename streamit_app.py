0343983c447cc4f93c552e75d83fdaa34adfdba9
gjperrreault <144843358+gjperrreault@users.noreply.github.com>
2023-09-26 14:06:57 -0600

    Update streamit_app.py

--- streamit_app.py
+++ streamit_app.py
@@ -29,4 +29,5 @@ streamlit.text(fruityvice_response.json())
 
 # take the json version of the response and normalize it 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
+# output it on the screen as a table
+streamlit.dataframe(fruityvice_normalized)
